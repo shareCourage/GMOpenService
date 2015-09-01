b14f24c4e248b151e5c9f1da1b5f65712cca16c6
brian80s <panhaohua2014@yahoo.com>
2015-09-01 09:09:49 +0800

    MBProgressHUD去除蒙版效果

--- New_Simplify/Classes/Tool/PHTest.h
+++ New_Simplify/Classes/Tool/PHTest.h
@@ -1,13 +1,13 @@
 //
-//  PHTest.m
+//  PHTest.h
 //  New_Simplify
 //
 //  Created by Kowloon on 15/8/31.
 //  Copyright (c) 2015年 Goome. All rights reserved.
 //
 
-#import "PHTest.h"
+#import <Foundation/Foundation.h>
 
-@implementation PHTest
+@interface PHTest : NSObject
 
 @end
